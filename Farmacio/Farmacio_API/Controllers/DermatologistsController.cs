2a51faf6d6afc3c53ad5735d53a30051e8bc1c1c
Luka Bjelica <bjelicaluka99@gmail.com>
2021-03-27 18:27:28 +0100

    feat: added DermatologistController

---
+++ Farmacio/Farmacio_API/Controllers/DermatologistsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using AutoMapper;
+using Farmacio_API.Contracts.Requests.Accounts;
+using Farmacio_Models.Domain;
+using Farmacio_Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farmacio_API.Controllers
+{
+    [ApiController]
+    [Route("pharmacists")]
+    [Produces("application/json")]
+    public class DermatologistsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IDermatologistService _dermatologistService;
+        
+        public DermatologistsController(IDermatologistService dermatologistService, IMapper mapper)
+        {
+            _dermatologistService = dermatologistService;
+            _mapper = mapper;
+        }
+        
+        /// <summary>
+        /// Reads all existing dermatologists in the system.
+        /// </summary>
+        /// <response code="200">Read dermatologists.</response>
+        [HttpGet]
+        public IActionResult GetDermatologists()
+        {
+            return Ok(_dermatologistService.Read());
+        }
+        
+        /// <summary>
+        /// Search all existing dermatologists in the system.
+        /// </summary>
+        /// <response code="200">Searched dermatologists.</response>
+        [HttpGet("search")]
+        public IActionResult SearchDermatologists(string name)
+        {
+            return Ok(_dermatologistService.SearchByName(name));
+        }
+        
+        /// <summary>
+        /// Reads an existing dermatologist in the system.
+        /// </summary>
+        /// <response code="200">Read dermatologist.</response>
+        [HttpGet("{id}")]
+        public IActionResult GetDermatologist(Guid id)
+        {
+            return Ok(_dermatologistService.Read(id));
+        }
+    }
+}
