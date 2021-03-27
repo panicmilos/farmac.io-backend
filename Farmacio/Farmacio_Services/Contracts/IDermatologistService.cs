861fe252bd55b921285d5a5169151bdd22cff2f2
Luka Bjelica <bjelicaluka99@gmail.com>
2021-03-27 18:23:37 +0100

    feat: added Dermatologists Service

---
+++ Farmacio/Farmacio_Services/Contracts/IDermatologistService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Farmacio_Models.Domain;
+
+namespace Farmacio_Services.Contracts
+{
+    public interface IDermatologistService : IAccountService
+    {
+        IEnumerable<Account> ReadForPharmacy(Guid pharmacyId);
+        Account ReadForPharmacy(Guid pharmacyId, Guid pharmacistId);
+        IEnumerable<Account> SearchByNameForPharmacy(Guid pharmacyId, string name);
+    }
+}
---
+++ Farmacio/Farmacio_Services/Implementation/DermatologistService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Farmacio_Models.Domain;
+using Farmacio_Repositories.Contracts.Repositories;
+using Farmacio_Services.Contracts;
+
+namespace Farmacio_Services.Implementation
+{
+    public class DermatologistService : AccountService, IDermatologistService
+    {
+        public DermatologistService(IEmailVerificationService emailVerificationService, IRepository<Account> repository) : base(emailVerificationService, repository)
+        {
+        }
+        
+        public override IEnumerable<Account> Read()
+        {
+            return base.Read().Where(account => account.Role == Role.Dermatologist).ToList();
+        }
+
+        public override Account Read(Guid id)
+        {
+            var account = base.Read(id);
+
+            return account?.Role == Role.Dermatologist ? account : null;
+        }
+
+        public IEnumerable<Account> ReadForPharmacy(Guid pharmacyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account ReadForPharmacy(Guid pharmacyId, Guid pharmacistId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> SearchByNameForPharmacy(Guid pharmacyId, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
