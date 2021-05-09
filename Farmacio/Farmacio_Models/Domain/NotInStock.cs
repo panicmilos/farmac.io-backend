17397e389aeecb3cac3e9bb70b418d093f4b79f9
Luka Bjelica <bjelicaluka99@gmail.com>
2021-05-09 22:50:39 +0200

    feat: implemented not in stock service - read and mar seen

--- Farmacio/Farmacio_Models/Domain/NotInStock.cs
+++ Farmacio/Farmacio_Models/Domain/NotInStock.cs
@@ -5,6 +5,7 @@ namespace Farmacio_Models.Domain
     public class NotInStock : BaseEntity
     {
         public Guid MedicineId { get; set; }
+        public virtual Medicine Medicine { get; set; }
         public Guid PharmacyId { get; set; }
         public bool IsSeen { get; set; }
     }
---
+++ Farmacio/Farmacio_Services/Contracts/INotInStockService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Farmacio_Models.Domain;
+using Farmacio_Models.DTO;
+using System.Collections.Generic;
+
+namespace Farmacio_Services.Contracts
+{
+    public interface INotInStockService : ICrudService<NotInStock>
+    {
+        IEnumerable<NotInStock> ReadFor(Guid pharmacyId);
+        NotInStock MarkSeen(Guid notInStockId);
+    }
+}
