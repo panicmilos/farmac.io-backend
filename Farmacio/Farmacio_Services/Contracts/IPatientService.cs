18d95cd6534bbfd6853ccd232bc5adb3e6158007
Panic <panic.milos99@gmail.com>
2021-05-14 22:22:41 +0200

    feat: adding loyalty points after sucessfull appointment or issuing medicines.

--- Farmacio/Farmacio_Services/Contracts/IPatientService.cs
+++ Farmacio/Farmacio_Services/Contracts/IPatientService.cs
@@ -6,7 +6,11 @@ namespace Farmacio_Services.Contracts
     public interface IPatientService : IAccountService
     {
         bool HasExceededLimitOfNegativePoints(Guid patientId);
+
         void DeleteNegativePoints();
+
         Account UpdateLoyaltyProgram(Guid patientAccountId, Guid? loyaltyProgramId);
+
+        Account UpdateLoyaltyPoints(Guid patientUserId, int forPoints);
     }
 }
